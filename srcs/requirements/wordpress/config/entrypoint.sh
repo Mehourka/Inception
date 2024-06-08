aaf397ba17c2a0415ce04131db5528ce11d94ad5
Karim <k.mehour@gmail.com>
2024-06-08 16:40:42 -0400

    Refactor, and remove unused wp-config.php

--- srcs/requirements/wordpress/config/entrypoint.sh
+++ srcs/requirements/wordpress/config/entrypoint.sh
@@ -13,14 +13,12 @@ while ! mysqladmin ping -h "$DB_HOST";do
 	echo "[ENTRYPOINT] Connecting to database ..."
   sleep 1;
 done
-# 	echo "[ENTRYPOINT] Connecting to database ..."
-# 	echo --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD
-# 	sleep 1
-# done
-wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD
-echo --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD
-wp core install --url=$HOST_42 --title=$HOST_42 --admin_user=$WP_SUPER_USER --admin_email=$WP_SUPER_EMAIL --admin_password=$WP_SUPER_PASSWORD
 
+# Create wordpress wp-config.php
+wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD
+
+# Create WordPress Users
+wp core install --url=$HOST_42 --title=$HOST_42 --admin_user=$WP_SUPER_USER --admin_email=$WP_SUPER_EMAIL --admin_password=$WP_SUPER_PASSWORD
 wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD
 
 exec $@
