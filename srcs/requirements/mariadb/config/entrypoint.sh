a004ac2f4694ee36ca9ef06cf20c537baf09908f
Karim Mehour <kmehour@c2r5p3.42quebec.com>
2024-06-10 18:34:42 -0400

    mariadb Done

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -6,11 +6,26 @@ else
  mysql_install_db --user=mysql --ldata=/var/lib/mysql
 fi
 
+# Config MySQL
+usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 << EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 
-# usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 << EOF
-# CREATE DATABASE ${DB_NAME};
-# CREATE USER '${DB_USER}'@'localhost' identified by '${DB_PASSWORD}';
-# EOF
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+# Configure file
+cat << EOF > /etc/my.cnf.d/mariadb-server.cnf
+[mysqld]
+datadir=/var/lib/mysql
+socket=/run/mysql/mysqld.sock
+bind-address=*
+port=3306
+user=mysql
+EOF
+# exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
 
-exec "$@"
+exec mysqld_safe 
